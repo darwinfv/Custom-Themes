168f956c0890c495cc305b4dfe93af1d53b77eab
Darwin Vaz <dvaz@purdue.edu>
2020-09-12 19:31:24 -0400

    completed color changing

--- color.js
+++ color.js
@@ -1,7 +1,11 @@
 document.getElementById('bg').addEventListener('input', () => {
-    document.body.style.backgroundColor = bg.value;
+    chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor = "' + bg.value + '";'
+    });
 });
 
 document.getElementById('text').addEventListener('input', () => {
-    document.body.style.color = text.value;
+    chrome.tabs.executeScript({
+        code: 'document.body.style.color = "' + text.value + '";'
+    });
 });
